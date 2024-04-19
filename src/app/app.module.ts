a968eb7c245e4a44d356141132f42cf6402a67e3
Caio Ruiz <cerz@gft.com>
2024-04-18 22:13:54 -0300

    Home sem imagem, projetos em breve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {MatIconModule} from "@angular/material/icon";
+import { HeaderComponent } from './components/header/header.component';
+import { ProjetosComponent } from './components/projetos/projetos.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProjetosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
